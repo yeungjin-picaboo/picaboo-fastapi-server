a1dcc90e256a50530eb37729165d0a8266593b91
Koo <dg.changeworld@gmail.com>
2023-04-25 11:24:00 +0900

    Feat: AWS S3 connected & save image in Se

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import FileResponse
 from PIL import Image
 from dotenv import load_dotenv
@@ -6,6 +6,8 @@ from stability_sdk import client
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import os 
+import boto3
+import logging
 import io
 import openai
 import psycopg2
@@ -15,6 +17,8 @@ import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 
 app = FastAPI()
 
+logging.basicConfig(level=logging.DEBUG)
+
 load_dotenv(verbose=True)
 
 openai.api_key= os.getenv('OPENAPI_KEY')
@@ -22,20 +26,22 @@ openai.api_key= os.getenv('OPENAPI_KEY')
 os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
 os.environ['STABILITY_KEY'] = os.getenv('STABILITY_KEY')
 
-#DB 나중에 다 파일분리
-DB_USERNAME = os.getenv('DB_USERNAME')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_NAME = os.getenv('DB_NAME')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
+#AWS
+def s3_connection():
+    try:
+        s3 = boto3.client(
+            service_name="s3",
+            region_name="ap-northeast-2",
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+        )
+    except Exception as e:
+        print(e)
+    else:
+        print("s3 bucket connected!")
+        return s3
+s3 = s3_connection()
 
-conn = psycopg2.connect(
-    dbname= DB_NAME,
-    user=DB_USERNAME,
-    password=DB_PASSWORD,
-    host=DB_HOST,
-    port=DB_PORT
-)
 
 #CORS
 app.add_middleware(
@@ -52,8 +58,13 @@ stability_api = client.StabilityInference(
     engine="stable-diffusion-xl-beta-v2-2-2",
     )
 
+
 class Diary(BaseModel):
     content: str
+
+class requestData(BaseModel):
+    prompt: str
+    userId: int
 
 @app.get("/")
 def root_main():
@@ -61,27 +72,27 @@ def root_main():
 
 @app.get('/api/diaries/emotion/{content}')
 def summarize_diary(content: str):
-    response_summary = openai.Completion.create(
-        model="text-davinci-002",
-        prompt=f"Summarize the following text: '{content}'.",
-        max_tokens=50,
-    )
-    summary = response_summary.choices[0].text.strip()
+    #response_summary = openai.Completion.create(
+    #    model="text-davinci-002",
+    #    prompt=f"Summarize the following text: '{content}'.",
+    #    max_tokens=50,
+    #)
+    #summary = response_summary.choices[0].text.strip()
 
     response_emotion = openai.Completion.create(
         model="text-davinci-002",
         prompt=f"Extract emotion from the following text: '{content}'.",
-        max_tokens=30,
+        max_tokens=5,
     )
 
     emotion = response_emotion.choices[0].text.strip()
+    return {'emotion':emotion}
+    #return {'summary': summary, 'emotion': emotion}
 
-    return {'summary': summary, 'emotion': emotion}
-
-@app.get("/api/diaries/picture/{prompt}")
-def make_picture(prompt:str):
+@app.get("/api/diaries/picture/{content}")
+def make_picture(content:str):
     answers = stability_api.generate(
-    prompt=prompt,
+    prompt=content,
     seed=992446758, 
     steps=30,
     cfg_scale=8.0,      
@@ -102,7 +113,10 @@ def make_picture(prompt:str):
                 filename = str(uuid.uuid4()) + ".png"
                 filepath = os.path.join('img', filename)
                 img.save(filepath)
-                imageUrl = f'http://picaboonftimage.s3.ap-northeast-2.amazonaws.com/{filename}'
-    # return FileResponse(filename)
-    return imageUrl
+                try:
+                    s3.upload_file(filepath,"picaboodiaryimage",filename)
+                except Exception as e:
+                    print(e)
+    
+    return filename
 
